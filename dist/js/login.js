900122c065bda9a2d7175a8b3173aa9fb60d6a16
Phillip Matheson <pmatheson@dxc.com>
2020-06-25 07:39:53 +1000

    2020-06-25-07-39

--- dist/js/login.js
+++
@@ -1,6 +0,0 @@
-$('body').on('click', '#loginButton',function (e) {
-    e.preventDefault();
-    loggedInUserId = $('#loggedInUserId').val();
-    console.log('UserID: ' + loggedInUserId)
-    window.location.href = "index.html"
-});
--- sync2s3.bat
+++
@@ -1,2 +0,0 @@
-aws s3 sync . s3://new-bucket-5784d976 --delete
-pause
